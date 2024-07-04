dc73a06d2f91be8b1d903fe28860982bd19824fc
shantanuFyndGit <shantanuradke@fynd-external.com>
2024-07-04 16:36:50 +0530

    minor fix

--- constants.js
+++ constants.js
@@ -2,6 +2,6 @@ const packageInfo = require("./package.json");
 const integrationVersion = packageInfo.version;
 const zapierCoreVersion = packageInfo.dependencies["zapier-platform-core"];
 
-const INTEGRATION_PLATFORM = `Watermarkremover/${integrationVersion} (Zapier/${zapierCoreVersion})`;
+const INTEGRATION_PLATFORM = `WatermarkremoverIntegration/${integrationVersion} (Zapier/${zapierCoreVersion})`;
 
 module.exports = INTEGRATION_PLATFORM;
